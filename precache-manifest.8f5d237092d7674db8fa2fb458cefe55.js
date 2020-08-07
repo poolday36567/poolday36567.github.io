1d5ca0a46fd10645af32fa8b049fee6182f56fb1
JD <jd.dev10@gmail.com>
2020-08-06 20:57:54 -0400

    Updates

---
+++ precache-manifest.8f5d237092d7674db8fa2fb458cefe55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b495fca31d0c8ce3972d32c84c237d01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "023cfcf2f6fdc3fbb60f",
+    "url": "/static/css/main.a4e6bbc3.chunk.css"
+  },
+  {
+    "revision": "fcebf1c6e96323ab0dac",
+    "url": "/static/js/2.dd0c0778.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dd0c0778.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "023cfcf2f6fdc3fbb60f",
+    "url": "/static/js/main.9a5f8232.chunk.js"
+  },
+  {
+    "revision": "c5021e740bf1509977d7",
+    "url": "/static/js/runtime-main.7918ed71.js"
+  }
+]);
